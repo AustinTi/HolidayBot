79c1c495e1b924b9a2c6f8654f0179b374e1ce9e
Austin <austin.jpw@live.com>
2020-05-18 21:01:24 -0700

    HolidayBot Go!
    HolidayBot has been rewritten in Go! The JS code will be available at https://github.com/AustinTi/HolidayBot-JS

---
+++ commands/setReset.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"github.com/AustinTi/HolidayBot/db"
+	"github.com/AustinTi/HolidayBot/misc"
+)
+
+func setReset(p Params) bool {
+	channel, err := misc.GetDefaultChannel(p.Client, p.Guild)
+	if err != nil {
+		return false
+	}
+
+	db.UpdateAdult(p.Guild.ID, false)
+	db.UpdateCommand(p.Guild.ID, true)
+	db.UpdateDaily(p.Guild.ID, true)
+	db.UpdateDailyChannel(p.Guild.ID, channel.ID)
+	db.UpdatePrefix(p.Guild.ID, []string{"h]"})
+	db.UpdateRegion(p.Guild.ID, misc.GetDefaultRegion(p.Guild))
+
+	return true
+}
---
+++ events/GuildCreate.go
@@ -0,0 +1,20 @@
+package events
+
+import (
+	"github.com/AustinTi/HolidayBot/db"
+	"github.com/AustinTi/HolidayBot/misc"
+	"github.com/bwmarrin/discordgo"
+)
+
+//GuildCreate event
+func GuildCreate(client *discordgo.Session, guild *discordgo.GuildCreate) {
+	for _, value := range GuildCache {
+		if guild.ID == value.ID {
+			return
+		}
+	}
+
+	db.CreateGuild(client, guild.Guild)
+
+	misc.Log(client, "", "info", "join", nil, guild.Guild, "")
+}
